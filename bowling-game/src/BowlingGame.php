3cf522a593fac1351c68fa951472b5b71aea80e3
Fernando Fernandez <ffmp777@yahoo.com>
2017-05-26 08:29:59 -0600

    finished functionality for this Kata

--- bowling-game/src/BowlingGame.php
+++ bowling-game/src/BowlingGame.php
@@ -1,12 +1,70 @@
 <?php
 
-class BowlingGame {
-	public function roll($roll)
+class BowlingGame 
+{
+	protected $rolls = [];
+
+	private function guardAgainstInvalidRoll($pins)
 	{
-		// todo
+		if ($pins < 0)
+		{
+			throw new InvalidArgumentException;
+		}
 	}
-	public function score()
+
+	private function isStrike($roll)
+		{
+			return $this->rolls[ $roll ] == 10;
+		}
+	private function strikeBonus($roll)
+		{
+			$this->rolls[$roll + 1] = $this->rolls[$roll + 2];			
+		}
+
+	private function isSpare($roll)
+		{
+			return $this->rolls[$roll] + $this->rolls[$roll + 1] == 10;
+		}
+
+	public function spareBonus($roll)
+		{
+			return $this->rolls[$roll + 2];	
+		}
+
+	private function getDefaultFrameScore($roll)
+		{
+			return $this->rolls[$roll] + $this->rolls[$roll + 1];
+		}
+
+	public function roll($pins)
+		{
+			$this->guardAgainstInvalidRoll($pins);
+			$this->rolls[] = $pins;
+		}
+	public function score() // Responsible for calculating the score.
 	{
-		return 0;
+		$score = 0;
+		$roll = 0;
+
+		for ($frame = 1; $frame <= 10; $frame++)
+		{
+			if ($this->isStrike($roll))
+			{
+				$score += 10 + $this->rolls[$roll +1] + $this->rolls[$roll + 2];
+				$roll += 1;
+
+				continue;
+			}
+			if ($this->isSpare($roll))
+			{
+				$score += 10 + $this->spareBonus($roll);
+			}
+			else
+			{
+				$score += $this->getDefaultFrameScore($roll);
+			}
+			$roll += 2;
+		}
+		return $score;
 	}
 }
